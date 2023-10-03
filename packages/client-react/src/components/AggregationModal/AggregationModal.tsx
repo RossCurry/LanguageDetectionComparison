a7e9a3f25e767283c1716cdfc1245834c6a44100
Ross Curry <ross.curry@maloon.de>
2023-10-03 13:54:08 +0200

    add basic table

--- packages/client-react/src/components/AggregationModal/AggregationModal.tsx
+++ packages/client-react/src/components/AggregationModal/AggregationModal.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { useReactTable, TableOptions, ColumnDef, createColumnHelper, getCoreRowModel, flexRender } from '@tanstack/react-table'
 import style from './AggregationModal.module.css'
-import { AggregateQuery } from '../../../../shared-utils/Types'
+import { AggregateQuery, AggregateResult } from '../../../../shared-utils/Types'
 
 
 export default function AggregationModal() {
-  const [aggregationResults, setAggregationResults] = useState<AggregateQuery | null>(null)
+  const [AggregateQuery, setAggregateQuery] = useState<AggregateQuery | null>(null)
   
   useEffect(() => {
     const getAggregationResults = async () => {
@@ -23,8 +24,8 @@ export default function AggregationModal() {
         console.error(error);
       }
       const asJson: AggregateQuery = await result?.json();
-      setAggregationResults(asJson);
-      console.log(asJson);
+      setAggregateQuery(asJson);
+      console.table(asJson.aggregateResults);
       // asJson.forEach((result) => {
       //   console.table(result);
       // });
@@ -34,7 +35,83 @@ export default function AggregationModal() {
   return (
     <div className={style.aggregationModalContainer}>AggregationModal
       {/* Add a cool table */}
-      {aggregationResults && <div>got some results</div>}
+      {AggregateQuery 
+        ? <TanTableReact aggregateResults={AggregateQuery.aggregateResults} />
+        : null
+      }
+    </div>
+  )
+}
+
+type TableHeaders = '_id'| 'avgProcessingTimeMs' | 'avgMatchesDeepL'
+type TableData = Pick<AggregateResult, TableHeaders>
+type TableProps = {
+  aggregateResults: AggregateQuery["aggregateResults"]
+}
+function TanTableReact({ aggregateResults } : TableProps) {
+  const tableData = aggregateResults;
+  const columnHelper = createColumnHelper<TableData>()
+  const columns = [
+    // Accessor Column - more advanced
+    columnHelper.accessor(row => row._id, {
+      id: 'serviceName',
+      cell: info => info.getValue(),
+      header: () => <span>Service</span>,
+    }),
+    columnHelper.accessor(row => row.avgMatchesDeepL, {
+      id: 'avgMatchesDeepL',
+      cell: info => info.getValue() + '%',
+      header: () => <span>Accuracy</span>,
+    }),
+    columnHelper.accessor(row => row.avgProcessingTimeMs, {
+      id: 'avgProcessingTimeMs',
+      cell: info => info.getValue().toFixed(4),
+      header: () => <span>Avg processing time (ms)</span>,
+    }),
+    // basic col header
+    // columnHelper.accessor('avgMatchesDeepL', {
+    //   cell: info => info.getValue() + '%',
+    // }),
+    
+  ]
+
+  const table = useReactTable({
+    data: tableData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className={style.tableContainer}>
+            <table>
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
