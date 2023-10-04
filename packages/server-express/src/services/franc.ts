96b57fb455d994f1df0e44cc4dd746ebcc972092
Ross Curry <ross.curry@maloon.de>
2023-10-04 13:50:42 +0200

    remove falty package / return promise all

--- packages/server-express/src/services/franc.ts
+++ packages/server-express/src/services/franc.ts
@@ -1,6 +1,5 @@
 import {franc, francAll, Options} from 'franc'
 import { parseHrTime } from './deepl.js';
-import parseIso3to1 from 'iso-639-3-to-1';
 import { TranslationResult } from '../utils/shared-types.js';
 import { iso6393To1 } from '../utils/iso-639-3-to-1.js';
 
@@ -13,16 +12,8 @@ export default function detectFranc(text:string, completeResults: boolean = fals
   const startTime = process.hrtime()
   const resultIso639 = franc(text, francOptions);
   const timeDiff = process.hrtime(startTime)
-  let iso639One: string | undefined;
-  try {
-    iso639One = parseIso3to1(resultIso639) 
-  } catch (error) {
-    console.warn('Error trying to parse ISO-639-1 codes using iso-639-3-to-1 library')
-  } 
-
-  if (!iso639One){
-    iso639One = iso6393To1[resultIso639 as keyof typeof iso6393To1] || resultIso639
-  }
+  const iso639One = iso6393To1[resultIso639 as keyof typeof iso6393To1] || resultIso639
+  
   return {
     confidence: null,
     detectedLang: iso639One,
